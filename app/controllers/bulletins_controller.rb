6f0e6c57989427ccec9b22c77ef8654e8501c2cd
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-05-08 03:04:43 +0800

    Add Controller Bulletins

---
+++ app/controllers/bulletins_controller.rb
@@ -0,0 +1,38 @@
+class BulletinsController < ApplicationControlle
+
+  before_action :find_bulletin, :only => [ :show, :edit, :update ]
+
+  def index
+    @bulletins = Bulletin.all
+  end
+
+  def new
+    @bulletin = Bulletin.new
+  end
+
+  def create
+    @bulletin = Bulletin.new(bulletin_params)
+    if @bulletin.save
+      redirect_to bulletins_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def update
+    @bulletin.update(bulletin_params)
+    redirect_to bulletins_path
+  end
+
+  private
+  def bulletin_params
+    params.require(:bulletin).permit(:content)
+  end
+
+  def find_bulletin
+    @bulletin = Bulletin.find(params[:id])
+  end
+end
